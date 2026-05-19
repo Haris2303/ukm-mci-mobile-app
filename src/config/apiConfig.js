5fc4b0f3b86566869fbd9a00e667ca000b362ac6
haris2303 <hrsccf102@gmail.com>
2026-05-19 19:18:43 +0900

    change ipaddress

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -4,4 +4,4 @@
 // Cek dengan perintah "ipconfig" di CMD
 // Contoh: 'http://192.168.1.5:8000/api'
 // export const BASE_URL = "http://10.10.10.243/api";
-export const BASE_URL = "http://10.10.10.126/api";
+export const BASE_URL = "http://192.168.1.19/api";
