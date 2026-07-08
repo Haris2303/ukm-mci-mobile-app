3be4e51d7cd26755a38365d42e8a1bac445111e7
haris2303 <hrsccf102@gmail.com>
2026-07-08 21:53:28 +0900

    change ip address

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -3,4 +3,5 @@
 // ⚠️ GANTI dengan IP komputer Anda di jaringan WiFi!
 // Cek dengan perintah "ipconfig" di CMD
 // Contoh: 'http://192.168.1.5:8000/api'
-export const BASE_URL = "http://10.10.10.126/api";
+// export const BASE_URL = 'https://ukm-mci-digitalisasi.test/api';
+export const BASE_URL = 'http://10.99.167.57/api';
